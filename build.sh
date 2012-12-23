3e06b6943c49be47f7da9aff84b5279f25beb8a3
KOBAYASHI Daisuke <daisuke.koba@gmail.com>
2012-12-23 12:44:36 +0900

    Correct copyright year.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # ----------------------------------------------------------------------------
-# Copyright (c) 2011-2012, KOBAYASHI Daisuke
+# Copyright (c) 2012, KOBAYASHI Daisuke
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
